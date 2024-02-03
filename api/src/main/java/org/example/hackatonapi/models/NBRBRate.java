81424e649e6b18e79c858a47c81d9be98f886335
chechyotka <romanchechyotkin@gmail.com>
2024-02-03 12:02:19 +0300

    nbrb endpoints for currencies and rates

--- api/src/main/java/org/example/hackatonapi/models/NBRBRate.java
+++ api/src/main/java/org/example/hackatonapi/models/NBRBRate.java
@@ -10,44 +10,4 @@ public class NBRBRate {
     private String Cur_Abbreviation;
     private int Cur_Scale;
     private double Cur_OfficialRate;
-
-    public int getCur_ID() {
-        return Cur_ID;
-    }
-
-    public void setCur_ID(int cur_ID) {
-        Cur_ID = cur_ID;
-    }
-
-    public java.util.Date getDate() {
-        return Date;
-    }
-
-    public void setDate(java.util.Date date) {
-        Date = date;
-    }
-
-    public String getCur_Abbreviation() {
-        return Cur_Abbreviation;
-    }
-
-    public void setCur_Abbreviation(String cur_Abbreviation) {
-        Cur_Abbreviation = cur_Abbreviation;
-    }
-
-    public int getCur_Scale() {
-        return Cur_Scale;
-    }
-
-    public void setCur_Scale(int cur_Scale) {
-        Cur_Scale = cur_Scale;
-    }
-
-    public double getCur_OfficialRate() {
-        return Cur_OfficialRate;
-    }
-
-    public void setCur_OfficialRate(double cur_OfficialRate) {
-        Cur_OfficialRate = cur_OfficialRate;
-    }
 }
