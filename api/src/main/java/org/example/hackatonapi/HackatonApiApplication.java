4b4711bd6ac265a08ab9542c9eca6731756918b8
moto_default_motive <mihail.stepushin@gmail.com>
2024-02-03 10:55:46 +0300

    moved project into special folder & created folder for telegram project

---
+++ api/src/main/java/org/example/hackatonapi/HackatonApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.hackatonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackatonApiApplication.class, args);
+    }
+
+}
---
+++ api/src/test/java/org/example/hackatonapi/HackatonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hackatonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
