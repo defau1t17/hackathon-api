30ae2ae73fe9a9bbc5f314221dba7bbc50aad439
Stepan Belko <Stpn.Belko@gmail.com>
2024-02-03 15:08:50 +0300

    bot v3

---
+++ api/src/main/java/org/example/hackatonapi/model/State.java
@@ -0,0 +1,9 @@
+package org.example.hackatonapi.model;
+
+import lombok.Data;
+
+@Data
+public class State {
+    private String bankName;
+    private String currencyName;
+}
--- api/src/main/java/org/example/hackatonapi/service/TelegramBot.java
+++ api/src/main/java/org/example/hackatonapi/service/TelegramBot.java
@@ -5,7 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.example.hackatonapi.config.BotConfig;
 import org.example.hackatonapi.menu.ActionMenu;
 import org.example.hackatonapi.menu.CurrencyMenu;
-import org.example.hackatonapi.menu.Menu;
+import org.example.hackatonapi.menu.DateMenu;
+import org.example.hackatonapi.model.State;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
@@ -18,13 +19,14 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.Keyboard
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 
 @Slf4j
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
-    private Menu currencyMenu;
+    private static HashMap<Long, State> map = new HashMap<>();
     final BotConfig config;
 
     static final String HELP_TEXT = "hackaton java team 12bot" +
@@ -63,7 +65,12 @@ public class TelegramBot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
             String messageText = update.getMessage().getText();
-            long chatId = update.getMessage().getChatId();
+            Long chatId = update.getMessage().getChatId();
+            if (chatId != null && !map.containsKey(chatId)) {
+                map.put(chatId, new State());
+            }
+
+
             switch (messageText) {
                 case "/start" -> {
 //                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
@@ -76,6 +83,7 @@ public class TelegramBot extends TelegramLongPollingBot {
                     sendMessage(message);
                 }
                 case "Беларусбанк" -> {
+                    map.get(chatId).setBankName("Беларусбанк");
                     SendMessage message = CurrencyMenu.getMenu();
                     message.setText("Беларусбанк");
                     message.setChatId(chatId);
@@ -83,16 +91,45 @@ public class TelegramBot extends TelegramLongPollingBot {
                 }
 
                 case "Альфа банк" -> {
+                    map.get(chatId).setBankName("Альфа банк");
                     SendMessage message = CurrencyMenu.getMenu();
+                    message.setText("Альфа банк");
                     message.setChatId(chatId);
                     sendMessage(message);
                 }
 
                 case "НБРБ" -> {
+                    map.get(chatId).setBankName("НБРБ");
                     SendMessage message = CurrencyMenu.getMenu();
+                    message.setText("НБРБ");
                     message.setChatId(chatId);
                     sendMessage(message);
                 }
+
+                case "USD" -> {
+                    map.get(chatId).setCurrencyName("USD");
+                    SendMessage message = DateMenu.getMenu();
+                    message.setChatId(chatId);
+                    message.setText("u");
+                    sendMessage(message);
+                }
+                case "EUR" -> {
+                    map.get(chatId).setCurrencyName("EUR");
+                    SendMessage message = DateMenu.getMenu();
+                    message.setChatId(chatId);
+                    message.setText("e");
+                    sendMessage(message);
+                }
+                case "RUB" -> {
+                    map.get(chatId).setCurrencyName("RUB");
+                    SendMessage message = DateMenu.getMenu();
+                    message.setChatId(chatId);
+                    message.setText("r");
+                    sendMessage(message);
+                }
+
+                case "Курс на выбранный день" -> {}
+                case "Курс на текущий день" -> {}
                 default -> {
                     sendMessage(null);
                 }
@@ -100,51 +137,11 @@ public class TelegramBot extends TelegramLongPollingBot {
         }
     }
 
-//    private void register(long chatId) {
-//        SendMessage message = new SendMessage();
-//        message.setChatId(chatId);
-//        message.setText("Do you want really to register?");
-//
-//        InlineKeyboardMarkup markupInLine = new InlineKeyboardMarkup();
-//        List<List<InlineKeyboardButton>> rowsInLine = new ArrayList<>();
-//        List<InlineKeyboardButton> rowInLine = new ArrayList<>();
-//
-//        var yesButton = new InlineKeyboardButton();
-//        yesButton.setText("Yes");
-//        yesButton.setCallbackData("YES_BUTTON");
-//
-//        var noButton = new InlineKeyboardButton();
-//        noButton.setText("No");
-//        noButton.setCallbackData("NO_BUTTON");
-//
-//        rowInLine.add(yesButton);
-//        rowInLine.add(noButton);
-//
-//        rowsInLine.add(rowInLine);
-//
-//        markupInLine.setKeyboard(rowsInLine);
-//
-//        message.setReplyMarkup(markupInLine);
-//
-//        try {
-//            execute(message);
-//        } catch (TelegramApiException e) {
-////            log.error("Error occurred: " + e.getMessage());
-//        }
-//    }
-
-//    private void startCommandReceived(long chatId, String name) {
-//        String answer = EmojiParser.parseToUnicode("Привет, " + name + "! Чтобы воспользоваться функционалом " +
-//                "бота сперва выбери банк из меню снизу");
-//        sendMessage(chatId, answer);
-//    }
 
     private void sendMessage(SendMessage message) {
         try {
-            System.out.println(message.getText());
             execute(message);
         } catch (TelegramApiException e) {
-//            log.error("Error occurred: " + e.getMessage());
         }
     }
 }
